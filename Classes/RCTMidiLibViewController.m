669348a7eb3f2c57a3424720f649276ff3eb6373
recotana <info@recotana.com>
2011-01-17 16:26:33 +0900

    2nd test commit

--- Classes/RCTMidiLibViewController.m
+++ Classes/RCTMidiLibViewController.m
@@ -21,7 +21,7 @@
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        // Custom initialization 
     }
     return self;
 }
